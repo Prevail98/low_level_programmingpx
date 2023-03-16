fe067507c7d66b67ef8c55ca7408acf9acb971b2
prevail98 <prevailoyedepo5@gmail.com>
2023-03-16 12:01:35 -0700

    free

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point
+ *
+ * Return: this program returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
