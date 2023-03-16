45ae231e5832acb929e9b2688a9fb9d4a5bc65d6
prevail98 <prevailoyedepo5@gmail.com>
2023-03-16 11:36:03 -0700

    love

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - where the program starts
+ *
+ * Return - value is 0
+ */
+
+int main(void) 
+{
+        puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
