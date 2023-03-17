c02508e91812edf9853a94e5291f3ae3ae560a0b
prevail98 <prevailoyedepo5@gmail.com>
2023-03-17 15:52:30 -0700

    lovwerw

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main starts the program
+ *
+ * Return: return only 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d \n", i);
+	return (0);
