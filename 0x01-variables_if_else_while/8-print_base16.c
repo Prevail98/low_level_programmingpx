4d3675d7ef510299c0d9751c0160bf3795d939b2
prevail98 <prevailoyedepo5@gmail.com>
2023-03-19 14:58:03 -0700

    dreer

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,9 @@ int main(void)
 
 	char i;
 
-	for (i = '0'; i <= 'f'; i++)
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
