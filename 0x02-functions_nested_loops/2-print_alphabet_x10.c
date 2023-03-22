e46be86d3747c3012a7ce56a11630b3f1050eb8f
prevail98 <prevailoyedepo5@gmail.com>
2023-03-22 05:59:52 -0700

    her

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10() - function that prints alphabet
+ * print_alphabet_x10 - function that prints alphabet
  *
  * Return: return only 0
  */
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	int n;
 	char i;
 
-	for (n = 0; n <= 10 ; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
