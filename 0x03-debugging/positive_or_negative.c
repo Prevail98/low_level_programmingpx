a40dc5053eb5c26601e11a2be1e6325360a9041d
Tim <prevailoyedepo5@gmail.com>
2023-03-23 14:15:34 +0000

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,15 +2,13 @@
 
 /* betty style doc for function main goes there */
 /**
- * positive_or_negative - single function is the starting point
- * @parameter: int i
- *
- * Return: void 
- */
-
+* positive_or_negative - single function is the starting point
+* @i: our number to test 
+*
+* Return: void 
+*/
 void positive_or_negative(int i)
 {
-	/* your code goes there */
 	if (i > 0)
 		printf("%d is positive\n", i);
 	else if (i <  0)
