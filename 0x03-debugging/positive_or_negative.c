477ed4ee23ce33fe4255f0e831d92bd65649d9c0
Tim <prevailoyedepo5@gmail.com>
2023-03-23 13:47:58 +0000

    Update positive_or_negative.c
    correct int bug

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,11 +9,11 @@
 void positive_or_negative(int i)
 {
 	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n <  0)
-		printf("%d is negative\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i <  0)
+		printf("%d is negative\n", i);
 	else
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	return (0);
 }
