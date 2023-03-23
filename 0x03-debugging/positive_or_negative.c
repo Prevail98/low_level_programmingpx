638dd7c03ffc9a91d38f3bea50a8392d9fc61f86
Tim <prevailoyedepo5@gmail.com>
2023-03-23 13:41:31 +0000

    added the positive_or_negative function

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,1 +1,19 @@
-#inlude 
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main is the starting point
+ *
+ * Return: Returns value
+ */
+
+void positive_or_negative(int i)
+{
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n <  0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
