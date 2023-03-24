45fc636001207b69e660c8b9968d55001b94f736
prevail98 <prevailoyedepo5@gmail.com>
2023-03-24 14:50:21 -0700

    re

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - check if a character is a digit
+ *
+ * @x: the number to be checked
+ *
+ * Return: return 0
+ */
+
+int _isdigit(int x)
+{
+
+	if(x >= 48 && x <= 57)
+	{
+		return (1);
+	}
+	return (0);
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int main(void);
 int _isupper(int c);
 void print_most_numbers(void);
 void more_numbers(void);
+int _isdigit(int c);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
