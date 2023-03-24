2a5c063e05649c358aefbd2b3bbb0c2aa820fd45
prevail98 <prevailoyedepo5@gmail.com>
2023-03-24 15:08:41 -0700

    lre

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - check if a character is a digit
+ *
+ * @x: the number to be checked
+ *
+ * @y: the number to be checked
+ *
+ * Return: return 0
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isupper(int c);
 void print_most_numbers(void);
 void more_numbers(void);
 int _isdigit(int c);
+int mul(int a, int b);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
