e312f4308d79a63e732c407feb26fe9e1f029e1c
prevail98 <prevailoyedepo5@gmail.com>
2023-03-24 13:40:37 -0700

    gre

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * isupper - check if a character is a digit
+ *
+ * @x: the number to be checked
+ *
+ * Return: return 0
+ */
+
+	Int _isupper(int x)
+	{
+
+	If(x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+	}
