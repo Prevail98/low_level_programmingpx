d9fc05d1da40a65e9a405b17558f55759a364f3f
prevail98 <prevailoyedepo5@gmail.com>
2023-03-27 10:32:15 -0700

    point

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes pointer to int
+ *
+ * @n: and input for pointer
+ *
+ * Return: nothing to return
+ *
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
