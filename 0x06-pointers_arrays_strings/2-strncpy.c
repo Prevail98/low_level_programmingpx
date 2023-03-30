570e38766cd7dc2407839a7c39ebd213b2826fcc
Tim <prevailoyedepo5@gmail.com>
2023-03-30 14:13:22 +0100

    2-strncpy
    newcomit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* strncpy - copies string
+*
+* @dest: destination string
+*
+* @src: source string
+*
+* @n: number of strings
+*
+* Return: destination string
+
+char strncpy(char *dest, char *src, int n)
+{
+      int i;
+
+      for (i=0; i < n; i++)
+
+      dest[i] = src[i];
+
+      return (dest);
+
+}
