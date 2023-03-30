9ddd77254be2ddaedf9884526cadf9da1f49c9fa
Tim <prevailoyedepo5@gmail.com>
2023-03-30 16:51:13 +0100

    4 ray
    Array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *reverse array - it reverses elements of an array
+ *
+ *@n:function paramater
+ *
+ *@a: function parameter
+ *
+ *Return: 0
+
+void reverse_array(int *a, int n)
+{
+int i; 
+int j;
+
+for (i=0; i< n/2; i++)
+
+j- a[i];
+
+a[1] = a[n-1- 1];
+a[ n - 1 - i ] = j;
+
+}
+
+
