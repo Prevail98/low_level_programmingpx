480a44343d69aa1059dacf322a5f400a47465f9e
prevail98 <prevailoyedepo5@gmail.com>
2023-04-17 15:59:46 -0700

    free

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - the main
+ *
+ * __FILE__ : a macro
+ *
+ * Return: the return value is 0
+ */
 
-int main()
+
+int main(void)
 {
 	printf("%s\n", __FILE__);
-	return 0;
+	return (0);
 }
