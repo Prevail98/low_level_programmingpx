a57e54e67148bfda4f119f849f4521842bf23dd0
Tim <prevailoyedepo5@gmail.com>
2023-04-21 23:12:55 +0100

    Update 2-main.c

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -1,32 +1,12 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * print_strings - function that prints strings, followed by a new line
- * @separator: pointer to the separator
- * @n: const unsigned int passed from main,
- * count argument indicating the number
- * of variadic arguments
+ * main - check the code
  *
- * Return: void
+ * Return: Always 0.
  */
-
-void print_strings(const char *separator, const unsigned int n, ...)
+int main(void)
 {
-	va_list args;
-	unsigned int i;
-	char *tmp;
-
-	va_start(args, n);
-	for (i = 0; i < n; i++)
-	{
-		tmp = va_arg(args, char *);
-		printf("%s", tmp != NULL ? tmp : "(nil)");
-		if (i != n - 1)
-			if (separator)
-				printf("%s", separator);
-	}
-	printf("\n");
-	va_end(args);
+    print_strings(", ", 2, "Jay", "Django");
+    return (0);
 }
